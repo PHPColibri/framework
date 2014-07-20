dddf14fd59a6105574d9cf8f7601df21ac2c8d8d
alek13 <alek13.me@gmail.com>
2014-07-20 14:15:33 +0400

    first commit

---
+++ ICache.php
@@ -0,0 +1,42 @@
+<?php
+namespace Colibri\Cache;
+
+/**
+ *
+ * @author		Александр Чибрикин aka alek13 <chibrikinalex@mail.ru>
+ * @package		xTeam
+ * @subpackage	a13FW
+ * @version		1.0.0
+ */
+interface ICache
+{
+	/**
+	 * @param string $key for data
+	 * @return boolean result OR data
+	 */
+static
+	public function get($key);
+
+	/**
+	 * @param string $key for data
+	 * @param mixed $val any type of supported data: object, string, int…
+	 * @param int $expire seconds
+	 * @return boolean result
+	 */
+static
+	public function set($key, $value, $expire=null);
+
+	/**
+	 * @param	string	$key for data
+	 */
+static
+	public function delete($key);
+
+	/**
+	 * @param string $key for data
+	 * @param mixed $val any type of supported data: object, string, int…
+	 * @return boolean result OR data
+	 */
+static
+	public function remember($key, \Closure $getValueCallback, $expire = null);
+}
