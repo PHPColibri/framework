595a40b9d49085dc3e4dd97264c4c1502e35436f
alek13 <alek13.me@gmail.com>
2014-07-23 01:25:13 +0400

    first commit of the super old code

---
+++ IDynamicCollection.php
@@ -0,0 +1,13 @@
+<?php
+namespace Colibri\Base;
+
+/**
+ * 
+ * 
+ * @author		Александр Чибрикин aka alek13 <chibrikinalex@mail.ru>
+ * @package		xTeam
+ * @subpackage	a13FW
+ */
+interface IDynamicCollection extends \ArrayAccess, \Iterator, \Countable
+{
+};
