cd6ca84bef73ba38d7f9037de3e5506fecd8cf57
alek13 <alek13.me@gmail.com>
2014-07-23 01:41:12 +0400

    first commit

---
+++ PhpTemplate.php
@@ -0,0 +1,76 @@
+<?php
+namespace Colibri\View;
+
+use Colibri\Base\PropertyAccess;
+
+/**
+ *
+ *
+ * @author		Александр Чибрикин aka alek13 <chibrikinalex@mail.ru>
+ * @package		xTeam
+ * @subpackage	a13FW
+ * @version		1.01
+ *
+ * @property	string	filename
+ */
+class	PhpTemplate extends PropertyAccess
+{
+	/**
+	 * @var string	path/name of template
+	 */
+	protected	$_filename=null;
+	/**
+	 * @var array	variables of template for tpl compile
+	 */
+	public		$vars=array();
+
+
+	/**
+	 *
+	 * @param	string		$filename	имя файла
+	 */
+	public		function	__construct($filename=null)
+	{
+		if ($filename===null)	return;
+
+		$this->load($filename);
+	}
+	/**
+	 * Sets or adds variables of template (merge)
+	 * 
+	 * @param array $vars
+	 * @return static 
+	 */
+	public		function	setVars(array $vars)
+	{
+		$this->vars=array_merge($this->vars,$vars);
+		return $this;
+	}
+
+	public		function	load($filename=null)
+	{
+		if ($filename===null)			$filename=$this->_filename;
+		if ($filename===null)			$this->__raiseError(701);
+		if (!file_exists($filename))	$this->__raiseError(702,$filename);
+		$this->_filename=$filename;
+		return $this;
+	}
+	/**
+	 *
+	 * @return string compiled template text
+	 */
+	public		function	compile()
+	{
+		$__strCompiled__='';
+		
+		foreach ($this->vars as $key=>$value)
+			$$key=$value;
+		ob_start();
+		include($this->_filename);
+		$__strCompiled__=ob_get_contents();
+		ob_end_clean();
+
+		return $__strCompiled__;
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Colibri view component
+========================
+
