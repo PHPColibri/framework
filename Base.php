79335eb588ef024b5d69ced39f6c8c4c41eb880e
alek13 <alek13.me@gmail.com>
2014-07-23 01:44:38 +0400

    first commit

---
+++ Base.php
@@ -0,0 +1,50 @@
+<?php
+namespace Colibri\Controller;
+
+use Colibri\Base\PropertyAccess;
+
+/**
+ * base controller
+ *
+ * @author		Александр Чибрикин aka alek13 <chibrikinalex@mail.ru>
+ * @package		xTeam
+ * @version		1.01.0
+ *
+ * @property response
+ * @property string $division site division
+ * @property string $module
+ * @property string $method
+ */
+abstract
+class Base extends PropertyAccess
+{
+	protected	$_response=null;
+	protected	$_division=null;
+	protected	$_module=null;
+	protected	$_method=null;
+
+	final
+	public		function	__construct($division,$module,$method)
+	{
+		// $this->db=&$db;
+		$this->_division=$division;
+		$this->_module=$module;
+		$this->_method=$method;
+
+		$this->init();
+	}
+
+	protected	function	init()		{}
+
+	public		function	setUp()		{}
+						//  anyMethod() {}
+	public		function	tearDown()	{}
+
+	protected	function	done()		{}
+
+	final
+	public		function	__destruct()
+	{
+		$this->done();
+	}
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Colibri config component
+========================
+
