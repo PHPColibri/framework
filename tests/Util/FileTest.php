3d86202f9f15da609ae1dfde2e094ba4d0ac8656
alek13 <alek13.me@gmail.com>
2014-11-09 15:06:16 +0400

    file test

---
+++ tests/Util/FileTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Colibri\Tests\Util;
+
+use Colibri\Util\File;
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers       Colibri\Util\File::getMimeType
+	 */
+	public function testGetMimeType()
+	{
+		$mimeType = File::getMimeType(__DIR__ . '/FileTest.php');
+		$this->assertEquals('text/x-php', $mimeType);
+	}
+}
