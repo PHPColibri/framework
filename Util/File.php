e7e7fd19e15b2cc7f4a1bb6e6c18ff3a77db3422
alek13 <alek13.me@gmail.com>
2014-11-11 01:23:04 +0400

    remove php 5.3 from travis

--- Util/File.php
+++ Util/File.php
@@ -3,23 +3,26 @@ namespace Colibri\Util;
 /**
  * Description of CFile
  *
- * @author		Александр Чибрикин aka alek13 <alek13.me@gmail.com>
- * @version		1.00.0
+ * @author         Александр Чибрикин aka alek13 <alek13.me@gmail.com>
+ * @version        1.00.0
  */
 class File
 {
 	/**
 	 * Get mime-type og the file
-	 * @param string $filepath
+	 *
+	 * @param string $filePath
+	 *
+	 * @throws \Exception
 	 * @return string string with mime-type like 'image/jpeg' & etc.
 	 */
-static
-	public		function	getMimeType($filepath)
+	static
+	public function    getMimeType($filePath)
 	{
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-		$mime = finfo_file($finfo,$filepath);
-		finfo_close($finfo);
-		
+		$fInfo = finfo_open(FILEINFO_MIME_TYPE);
+		$mime = finfo_file($fInfo, $filePath);
+		finfo_close($fInfo);
+
 		return $mime;
 	}
 }
