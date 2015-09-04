5503a1a5011440eb3cbaced9c2a37defedccf071
Alexander Chibrikin <achibrikin@irr.ru>
2015-09-04 15:27:38 +0300

    add Enum

---
+++ Enum.php
@@ -0,0 +1,44 @@
+<?php
+namespace Colibri\Util;
+
+use Colibri\Pattern\Helper;
+
+/**
+ * Class Enum.
+ */
+abstract class Enum extends Helper
+{
+
+// @todo
+//	/**
+//	 * @var array cached valid values for classes
+//	 */
+//	protected static $validValues = [] или null;
+
+    /**
+     * @todo LLP: можно сделать "кеш", если это вообще хоть как-то ускорит (см. комменты)
+     * @return array
+     */
+    public static function getValidValues()
+    {
+        return /* <isset(self::$validValues[get_called_class()])> или <static::$validValues!==null> */
+            /* ? <self::$validValues[get_called_class()]> или <static::$validValues> : */
+            static::getConstList();
+    }
+    /**
+     * @param string $value
+     * @return bool
+     */
+    public static function isValid($value)
+    {
+        return in_array($value, static::getValidValues(), true);
+    }
+
+    /**
+     * @return array
+     */
+    protected static function getConstList()
+    {
+        return (new \ReflectionClass(get_called_class()))->getConstants();
+    }
+}
