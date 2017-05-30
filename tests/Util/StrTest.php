681c22229ce2178836560f312634a1b40b8323b0
Nikita Isakov <niki-isak@yandex.ru>
2017-05-30 20:17:15 +0300

    Using dataProviders

--- tests/Util/StrTest.php
+++ tests/Util/StrTest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Colibri\Tests\Util;
 
 use Colibri\Util\Str;
@@ -8,35 +7,69 @@ use PHPUnit_Framework_TestCase;
 class StrTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @covers \Colibri\Util\Str::isEmail()
+     * @dataProvider invalidEmailProvider
      */
-    public function testIsEmail()
+    public function testIsEmailNegative($email)
     {
-        $validEmails = [
-            'test@ya.ru',
-            'test-123@gmail.com',
-            'test.test@yahoo.com.ru',
-            'test@test.test.test.test',
-            '88005553535@ya.ru',
-            'xxxColibrixxx@gmail.com',
+        $this->assertFalse(Str::isEmail($email));
+    }
+
+    public function invalidEmailProvider()
+    {
+        return [
+            [
+                'invalidTest.test',
+            ],
+            [
+                '@ya.com.ru',
+            ],
+            [
+                'teststring',
+            ],
+            [
+                '88005553535@test',
+            ],
+            [
+                'test@colibri@test.ru',
+            ],
+            [
+                'test.@colibri.gmail',
+            ],
+            [
+                '.test@colibri.gmail',
+            ]
         ];
+    }
 
-        foreach ($validEmails as $email) {
-            $this->assertTrue(Str::isEmail($email));
-        }
+    /**
+     * @dataProvider validEmailProvider
+     */
+    public function testIsEmailPositive($email)
+    {
+        $this->assertTrue(Str::isEmail($email));
+    }
 
-        $invalidEmails = [
-            'invalidTest.test',
-            '@ya.com.ru',
-            'teststring',
-            '88005553535@test',
-            'test@colibri@test.ru',
-            'test.@colibri.gmail',
-            '.test@colibri.gmail',
+    public function validEmailProvider()
+    {
+        return [
+            [
+                'test@ya.ru',
+            ],
+            [
+                'test-123@gmail.com',
+            ],
+            [
+                'test.test@yahoo.com.ru',
+            ],
+            [
+                'test@test.test.test.test',
+            ],
+            [
+                '88005553535@ya.ru',
+            ],
+            [
+                'xxxColibrixxx@gmail.com',
+            ],
         ];
-
-        foreach ($invalidEmails as $email) {
-            $this->assertFalse(Str::isEmail($email));
-        }
     }
 }
