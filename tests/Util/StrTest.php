fcf722d5800e523390ef14b3bf0a93d93d865e1a
Nikita Isakov <niki-isak@yandex.ru>
2017-05-30 23:07:25 +0300

    fix codestyle

--- tests/Util/StrTest.php
+++ tests/Util/StrTest.php
@@ -4,10 +4,16 @@ namespace Colibri\Tests\Util;
 use Colibri\Util\Str;
 use PHPUnit_Framework_TestCase;
 
+/**
+ * @coversDefaultClass \Colibri\Util\Str
+ */
 class StrTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * @covers ::isEmail
      * @dataProvider invalidEmailProvider
+     *
+     * @param string $email
      */
     public function testIsEmailNegative($email)
     {
@@ -17,32 +23,21 @@ class StrTest extends PHPUnit_Framework_TestCase
     public function invalidEmailProvider()
     {
         return [
-            [
-                'invalidTest.test',
-            ],
-            [
-                '@ya.com.ru',
-            ],
-            [
-                'teststring',
-            ],
-            [
-                '88005553535@test',
-            ],
-            [
-                'test@colibri@test.ru',
-            ],
-            [
-                'test.@colibri.gmail',
-            ],
-            [
-                '.test@colibri.gmail',
-            ]
+            ['invalidTest.test'],
+            ['@ya.com.ru'],
+            ['teststring'],
+            ['88005553535@test'],
+            ['test@colibri@test.ru'],
+            ['test.@colibri.gmail'],
+            ['.test@colibri.gmail'],
         ];
     }
 
     /**
+     * @covers ::isEmail
      * @dataProvider validEmailProvider
+     *
+     * @param string $email
      */
     public function testIsEmailPositive($email)
     {
@@ -52,24 +47,12 @@ class StrTest extends PHPUnit_Framework_TestCase
     public function validEmailProvider()
     {
         return [
-            [
-                'test@ya.ru',
-            ],
-            [
-                'test-123@gmail.com',
-            ],
-            [
-                'test.test@yahoo.com.ru',
-            ],
-            [
-                'test@test.test.test.test',
-            ],
-            [
-                '88005553535@ya.ru',
-            ],
-            [
-                'xxxColibrixxx@gmail.com',
-            ],
+            ['test@ya.ru'],
+            ['test-123@gmail.com'],
+            ['test.test@yahoo.com.ru'],
+            ['test@test.test.test.test'],
+            ['88005553535@ya.ru'],
+            ['xxxColibrixxx@gmail.com'],
         ];
     }
 }
