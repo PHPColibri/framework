4b55013d4651ee1a83d19568809ab8189a3f5186
Alexander Chibrikin <alek13.me@gmail.com>
2017-09-03 00:06:12 +0000

    Apply fixes from StyleCI

--- Database/Query/Type.php
+++ Database/Query/Type.php
@@ -4,7 +4,7 @@ namespace Colibri\Database\Query;
 use Colibri\Util\Enum;
 
 /**
- * Type of SQL query (INSERT, SELECT, UPDATE, DELETE)
+ * Type of SQL query (INSERT, SELECT, UPDATE, DELETE).
  */
 class Type extends Enum
 {
