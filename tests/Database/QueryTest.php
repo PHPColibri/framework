553c0292d2571f8843c31b4fb27dd407a0517ae1
Alexander Chibrikin <alek13.me@gmail.com>
2017-09-17 22:52:37 +0000

    Apply fixes from StyleCI

--- tests/Database/QueryTest.php
+++ tests/Database/QueryTest.php
@@ -69,7 +69,6 @@ class QueryTest extends TestCase
 
     // -------------------------------------------------------------------------------------
 
-
     /**
      * @expectedException \InvalidArgumentException
      */
