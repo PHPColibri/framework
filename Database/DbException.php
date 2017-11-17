e370000d10128f3f1721340450648705b897b7fd
Alexander Chibrikin <alek13.me@gmail.com>
2017-11-17 14:20:20 +0300

    now DbException extend RuntimeException

--- Database/DbException.php
+++ Database/DbException.php
@@ -1,12 +1,12 @@
 <?php
 namespace Colibri\Database;
 
-use Exception as PhpException;
+use RuntimeException;
 
 /**
  * Если что-то пошло не так при подключении, при закрытии или ещё где-то, кроме выполнения запроса.
  * If something went wrong on connection, on close, or elsewhere except query execution.
  */
-class DbException extends PhpException
+class DbException extends RuntimeException
 {
 }
